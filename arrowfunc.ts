280f7b7ae1b0f62ed43643ff6b374e5567c203e1
supig <khacbaoanh.nguyen@gmail.com>
2017-06-28 21:21:53 +0200

    update learning typescript

---
+++ arrowfunc.ts
@@ -0,0 +1,6 @@
+var calculateSalary = function() {
+    return 10000;
+}
+let calculateSalaryArrow = () => 3000;
+console.log(calculateSalary())
+console.log(calculateSalaryArrow())
---
+++ dist/arrowfunc.js
@@ -0,0 +1,8 @@
+"use strict";
+var calculateSalary = function () {
+    return 10000;
+};
+var calculateSalaryArrow = function () { return 3000; };
+console.log(calculateSalary());
+console.log(calculateSalaryArrow());
+//# sourceMappingURL=arrowfunc.js.map
